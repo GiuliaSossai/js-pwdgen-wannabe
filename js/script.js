e6038af77bd32fee76a3ca97d5bf30fb6406e34f
GiuliaSossai <giulia.sossai@yahoo.it>
2021-10-20 15:09:14 +0200

    stampa password e lunghezza

--- js/script.js
+++ js/script.js
@@ -6,3 +6,27 @@
 // Stampa la lunghezza della password che è stata generata
 
 console.log('ciao');
+
+/*
+1 inizializzo variabili con nome cognome colore
+2 somma
+3 stampa video lunghezza password
+*/ 
+
+const firstName = prompt('Come ti chiami?');
+const surname = prompt('Qual è il tuo cognome?');
+const favColor = prompt('Qual  è il tuo colore preferito?');
+
+
+console.log(firstName);
+console.log(surname);
+console.log(favColor);
+
+document.getElementById("psw").innerHTML = 
+`
+Password: ${firstName}${surname}${favColor}
+`;
+
+let somma = firstName + surname + favColor;
+
+document.getElementById("lunghezza").innerHTML = somma.length;
