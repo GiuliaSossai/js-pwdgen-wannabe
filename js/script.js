a62a63d4dcc12ee3c8b807c5700575c17c1ffbfd
GiuliaSossai <giulia.sossai@yahoo.it>
2021-10-20 14:43:59 +0200

    init project

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Chiedi all’utente il suo nome,
+// poi chiedi il suo cognome,
+// poi chiedi il suo colore preferito
+// Infine scrivi sulla pagina nomecognomecolorepreferito21
+// BONUS
+// Stampa la lunghezza della password che è stata generata
+
+console.log('ciao');
